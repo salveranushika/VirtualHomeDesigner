bca703d2af3eb11c3532c43f7e4657ac2361428e
shivanagendrak <shivaaman751@gmail.com>
2024-12-14 20:13:03 -0600

    Initial Commit

---
+++ VirtualHomeDesigner/VirtualHomeDesignerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualHomeDesignerApp.swift
+//  VirtualHomeDesigner
+//
+//  Created by shiva on 12/14/24.
+//
+
+import SwiftUI
+
+@main
+struct VirtualHomeDesignerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
